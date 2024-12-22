fc17e8c5a0cc9dc16df581b56bfd8ede13028465
Lili <lili.berenyi@gmail.com>
2024-12-22 16:57:18 +0000

    Add solution to day 1, part 1

---
+++ advent-of-code/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using advent_of_code.december_1;
+
+Console.WriteLine("Hello, World!");
+
+Challenge1 challenge1 = new Challenge1();
+var result = challenge1.ProcessData(challenge1.GetData());
+Console.WriteLine(result);
---
+++ advent-of-code/december-1/Challenge1.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+
+namespace advent_of_code.december_1
+{
+    internal class Challenge1
+    {
+        public static Data GetData()
+        {
+            Data data = new Data();
+            string dataString = File.ReadAllText("Resources/challenge-1-input.txt");
+            var dataArray = dataString.Split('\n');
+            data.leftList = dataArray.Select(s => int.Parse(s.Split(' ')[0])).ToList();
+            data.rightList = dataArray.Select(s => int.Parse(s.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1])).ToList();
+            return data;
+        }
+
+        public int ProcessData(Data data)
+        {
+            data.leftList.Sort();
+            data.rightList.Sort();
+
+            var sumOfDistances = data.leftList.Select((x, i) => Math.Abs(data.leftList[i] - data.rightList[i])).Sum();
+
+            return sumOfDistances;
+
+        }
+    }
+
+    public class Data
+    {
+        public List<int> leftList = [];
+        public List<int> rightList = [];
+    }
+}
