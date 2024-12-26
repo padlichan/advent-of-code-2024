e75d196e12e87b8f533b9135caf14c8756c8e5b5
Lili <lili.berenyi@gmail.com>
2024-12-26 15:15:39 +0000

    Add solution to day 5, part 2

--- advent-of-code/december-5/Challenge5.cs
+++ advent-of-code/december-5/Challenge5.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Data;
+using System.Diagnostics;
 using System.Reflection.Metadata.Ecma335;
 
 namespace advent_of_code.december_5;
@@ -13,27 +15,69 @@ internal class Challenge5
     {
         updates = getUpdates();
         rules = getRules();
-        var correctUpdates = FilterUpdates(updates);
+        var correctUpdates = GetCorrectUpdates(updates);
         var sumOfMiddleNumbers = correctUpdates.Select(u => u.Where((n, i) => i == u.Length / 2).FirstOrDefault()).Sum();
         Console.WriteLine("December 5");
         Console.WriteLine($"The sum of middle numbers in correct updates: {sumOfMiddleNumbers}");
+        var incorrectUpdates = GetIncorrectUpdates(updates);
+        var sortedUpdates = incorrectUpdates.Select(u => SortUpdate(u));
+        var sumOfCorrectedMiddleNumbers = sortedUpdates.Select(u => u.Where((n, i) => i == u.Length / 2).FirstOrDefault()).Sum();
+        Console.WriteLine($"The sum of middle numbers in corrected updates: {sumOfCorrectedMiddleNumbers}\n");
     }
 
-    private List<int[]> FilterUpdates(List<int[]> updateList)
+    private List<int[]> GetCorrectUpdates(List<int[]> updateList)
     {
-        return updateList.Where(u => rules.All(r => CheckUpdateForRule(r, u))).ToList();
+        return updateList.Where(u => rules.All(r => CheckUpdateForRule(u, r))).ToList();
     }
 
-    private bool CheckUpdateForRule ((int,int) rule, int[] update)
+
+    private List<int[]> GetIncorrectUpdates(List<int[]> updateList)
     {
-        var indexOfSecond = update.Select((n,i) => (n,i)).Where(p => p.n == rule.Item2).Select(p => p.i).FirstOrDefault(-1);
+        return updateList.Where(u => !rules.All(r => CheckUpdateForRule(u, r))).ToList();
+    }
+
+    private int[] SortUpdate(int[] update)
+    {
+        Stopwatch timer = new Stopwatch();
+        timer.Start();
+
+        while(GetBrokenRules(update).Length > 0)
+        {
+            if (timer.ElapsedMilliseconds > 2000) break;
+            GetBrokenRules(update).ToList().ForEach(r => SortUpdateForRule(update, r));
+        }
+        timer.Stop();
+        return update;
+    }
+
+    private int[] SortUpdateForRule(int[] update, (int, int) rule)
+    {
+        var indexOfSecond = update.Select((n, i) => (n, i)).Where(p => p.n == rule.Item2).Select(p => p.i).FirstOrDefault(-1);
+        if (indexOfSecond == -1) return update;
+        var indexOfFirst = update.Select((n, i) => (n, i)).Where(p => p.n == rule.Item1).Select(p => p.i).FirstOrDefault(-1);
+        if (indexOfFirst < indexOfSecond) return update;
+
+        var temp = update[indexOfFirst];
+        update[indexOfFirst] = update[indexOfSecond];
+        update[indexOfSecond] = temp;
+        return update;
+    }
+
+    private (int, int)[] GetBrokenRules(int[] update)
+    {
+        return rules.Where(r => !CheckUpdateForRule(update, r)).ToArray();
+    }
+
+    private bool CheckUpdateForRule(int[] update, (int, int) rule)
+    {
+        var indexOfSecond = update.Select((n, i) => (n, i)).Where(p => p.n == rule.Item2).Select(p => p.i).FirstOrDefault(-1);
         if (indexOfSecond == -1) return true;
         var indexOfFirst = update.Select((n, i) => (n, i)).Where(p => p.n == rule.Item1).Select(p => p.i).FirstOrDefault(-1);
-        if(indexOfFirst < indexOfSecond) return true;
+        if (indexOfFirst < indexOfSecond) return true;
         return false;
     }
 
-    private (int,int)[] getRules()
+    private (int, int)[] getRules()
     {
         string data = File.ReadAllText(rulesPath);
         return data.Split(Environment.NewLine).Select(line => (int.Parse(line.Split('|')[0]), int.Parse(line.Split('|')[1]))).ToArray();
